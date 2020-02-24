8554fa21a7795bd8a1792b93c038df7534d963cd
Rolf Streefkerk <r.streefkerk@gmail.com>
2020-02-24 12:08:56 +0700

    fix variables

--- variables.tf
+++ variables.tf
@@ -23,30 +23,30 @@ variable "dynamodb_table_name" {
 }
 
 variable "hash_key" {
-  type        = "string"
+  type        = string
   description = "DynamoDB table Hash Key"
 }
 
 variable "hash_key_type" {
-  type        = "string"
+  type        = string
   default     = "S"
   description = "Hash Key type, which must be a scalar type: `S`, `N`, or `B` for (S)tring, (N)umber or (B)inary data"
 }
 
 variable "range_key" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "DynamoDB table Range Key"
 }
 
 variable "range_key_type" {
-  type        = "string"
+  type        = string
   default     = "S"
   description = "Range Key type, which must be a scalar type: `S`, `N`, or `B` for (S)tring, (N)umber or (B)inary data"
 }
 
 variable "attributes" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "Additional DynamoDB attributes in the form of a list of mapped values"
 }
@@ -80,23 +80,28 @@ variable "local_secondary_index_map" {
 # Variables: DynamoDB optional
 # -----------------------------------------------------------------------------
 variable "billing_mode" {
+  type    = string
   default = "PAY_PER_REQUEST"
 }
 
 variable "server_side_encryption" {
+  type    = bool
   default = false
 }
 
 variable "point_in_time_recovery" {
+  type    = bool
   default = false
 }
 
 variable "stream_enabled" {
+  type        = bool
   description = "Indicates whether Streams are to be enabled (true) or disabled (false)."
   default     = false
 }
 
 variable "stream_view_type" {
+  type        = string
   description = "When an item in the table is modified, StreamViewType determines what information is written to the table's stream. Valid values are KEYS_ONLY, NEW_IMAGE, OLD_IMAGE, NEW_AND_OLD_IMAGES."
   default     = null
 }
